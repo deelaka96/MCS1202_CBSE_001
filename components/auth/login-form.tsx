7cef9456c2f776744980c42c6e8e3cc18a67f363
Deelaka Algawatta <adeelaka@icptechno.com>
2023-04-28 23:04:52 +0530

    Login form validation

---
+++ components/auth/login-form.tsx
@@ -0,0 +1,85 @@
+import { Formik } from "formik";
+import styles from "./login-form.module.css";
+import { Button, Spinner, Form } from "react-bootstrap";
+import * as Yup from "yup";
+
+export default function LoginForm() {
+  const userValidationSchema = Yup.object({
+    username: Yup.string()
+      .required("Please provide an email")
+      .email("Invalid email format"),
+    password: Yup.string().required("Please provide a password"),
+  });
+
+  const handleSubmit = async (values: any) => {
+    console.log(values);
+  };
+
+  const initialValues = {
+    username: "",
+    password: "",
+  };
+
+  return (
+    <div className={styles.login_box + " p-3"}>
+      <h1 className="display-6 mb-3">Login</h1>
+      <Formik
+        validationSchema={userValidationSchema}
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+      >
+        {({
+          errors,
+          touched,
+          isSubmitting,
+          values,
+          handleChange,
+          handleSubmit,
+        }) => (
+          <Form noValidate onSubmit={handleSubmit} className="text-left">
+            <Form.Group controlId="username">
+              <Form.Label>Your Email </Form.Label>
+              <Form.Control
+                required
+                autoComplete="email"
+                type="email"
+                placeholder="john.doe@domain.com"
+                value={values.username}
+                onChange={handleChange}
+                isValid={touched.username && !errors.username}
+                isInvalid={touched.username && errors.username}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.username}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group controlId="password">
+              <Form.Label>Your Password</Form.Label>
+              <Form.Control
+                required
+                type="password"
+                autoComplete="current-password"
+                onChange={handleChange}
+                isValid={touched.password && !errors.password}
+                isInvalid={touched.password && errors.password}
+                placeholder="Password"
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.password}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <br />
+            <Button
+              variant="primary"
+              size="lg"
+              disabled={isSubmitting}
+              type="submit"
+            >
+              Login
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import LoginForm from '../components/login-form'
+import LoginForm from '../components/auth/login-form'
 
 const Home: NextPage = () => {
   return (
